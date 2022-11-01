71a859ae186fd43e3896b7d5401794c16d00bcbd
Noormoinddinhs <116093094+Noormoinddinhs@users.noreply.github.com>
2022-11-01 19:41:42 +0530

    Create n.py

---
+++ n.py
@@ -0,0 +1,26 @@
+rows=int(intput('enter the number'))
+for i in range(rows):
+  for j in range(i+1):
+    print(" * ",end='')
+    print("\n")
+    
+    rows=int(intput('enter the number'))
+for i in range(rows,0,-1):
+  for j in range(0,i):
+    print(" * ",end='')
+    print("\n")
+    
+    rows=int(intput('enter the number'))
+for i in range(rows):
+  for j in range(rows):
+    print(" # ",end='')
+    print("\n")
+    
+    
+    rows=int(intput('enter the number'))
+for i in range(rows+1,0,-1):
+  for j in range(0,i-1):
+    print(" * ",end='')
+    print("\n")
+    
+    
