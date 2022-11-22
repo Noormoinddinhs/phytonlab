ea4027aa362820648d65238686f4c90d594ecaa2
NoorMoinuddinHS <116093094+Noormoinddinhs@users.noreply.github.com>
2022-11-22 13:36:54 +0530

    Update a.py

--- a.py
+++ a.py
@@ -17,3 +17,13 @@ b=int(input('input base'))
 h=int(input('input height'))
 area = 1/2 * b * h
 print(area)
+
+# swape of a two number with using temporary variable
+
+a=int(input('1st number'))
+b=int(input('2nd number'))
+temp=a
+a=b
+b=temp
+print('a=' +str(a))
+print('b=' +str(b))
